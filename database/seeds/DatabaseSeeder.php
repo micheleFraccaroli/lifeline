e53b0253ae8812f859255e1e781a45df2c6a7caf
micheleFraccaroli <michelefraccaroli@gmail.com>
2017-12-11 16:43:01 +0100

    Controller users con modifica di web.php per routes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+    	$faker = Faker::create();
+    	foreach (range(1,10) as $index) {
+	        DB::table('users')->insert([
+	            'name' => str_random(10),
+	            'surname' => str_random(10),
+	            'email' => str_random(10).'@gmail.com',
+	            'sex' => 'M',
+	            'born' => '2017-12-12',
+	            'job' => str_random(10),
+	            'relation' => str_random(10),
+	            'image' => '0',
+	            'password' => bcrypt('secret'),
+	        ]);
+    	}
     }
 }
