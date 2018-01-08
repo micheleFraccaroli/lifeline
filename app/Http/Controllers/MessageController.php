203fe9cd20eb3302d1cdbefc0fd7cfcfb38bb920
micheleFraccaroli <michelefraccaroli@gmail.com>
2018-01-08 18:48:11 +0100

    Piccola correzione

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class MessageController extends Controller
 {
     protected function create(Request $request) {
-    	
+    	return Message::create([
+
+    	]);
     }
 }
