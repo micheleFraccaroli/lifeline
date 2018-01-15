e5fdd502e5bf3698e508488a4876d6027e066d0e
micheleFraccaroli <michelefraccaroli@gmail.com>
2018-01-15 14:38:46 +0100

    Piccole modifiche

--- resources/views/layouts/notifications/friendship_response.blade.php
+++ resources/views/layouts/notifications/friendship_response.blade.php
@@ -1,7 +1,7 @@
 <a href="/users/{{$notification->data['user']['my_id']}}">
 	
 	<?php if(!empty($notification->read_at) != 1) { ?>
-		<i><h5>Risposta alla di amicizia da {{ $notification->data['user']['name'] }} {{ $notification->data['user']['surname'] }}</h5></i> 
+		<h5>Risposta alla di amicizia da {{ $notification->data['user']['name'] }} {{ $notification->data['user']['surname'] }}</h5> <img src="green-dot.svg"> 
 	<?php } else { ?>
 		<h5>Risposta alla di amicizia da {{ $notification->data['user']['name'] }} {{ $notification->data['user']['surname'] }}</h5>
 	<?php } ?>
