8f94f9b81975599e2bd0581f0b9f41d05d6890fd
micheleFraccaroli <michelefraccaroli@gmail.com>
2018-01-16 15:11:26 +0100

    Risolto bug news richieste d'amicizia

--- public/js/news.js
+++ public/js/news.js
@@ -11,44 +11,44 @@ app.get('/', function(req, res){
   res.sendFile('/resources/index.blade.php');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket1){
 	console.log('other user connected to the notification for friendship request');
 	io.emit('new message', { 
-		username: socket.id,
+		username: socket1.id,
 		message: "sei connesso alle notifiche"
 	});
 
 	//set dentro alla var globale l'id del mio utente con cui sono loggato
-	socket.on('friend_identified', function(data) {
-		id_users[data.my_id] = socket.id;
+	socket1.on('friend_identified', function(data) {
+		id_users[data.my_id] = socket1.id;
 		console.log("MIO ID ========> " + data.my_id);
 		console.log("SOCKE MIA ===========> " + id_users[data.my_id]);
 	});
 
-	socket.on('friend_request', function(data) {
+	socket1.on('friend_request', function(data) {
 		console.log("RICHIESTA D'AMICIZIA DA UTENTE CON ID -------> " + data.user_requester);
 		console.log("RICHIESTA D'AMICIZIA PER UTENTE CON ID -------> " + data.user_receiver);
 		receiver_notification[data.user_requester] = data.user_receiver;
 		console.log(receiver_notification[data.my_identifier] = data.user_receiver);
 		console.log("SOCKET DEL RICEVENTE DALLA NOTIFICA: " + id_users[receiver_notification[data.user_requester]])
-		socket.to(id_users[receiver_notification[data.user_requester]]).emit('send_fr_req', {
+		socket1.to(id_users[receiver_notification[data.user_requester]]).emit('send_fr_req', {
 			news: "Richiesta di amicizia"
 		});
 	});
 
-	socket.on('friend_response', function(data) {
+	socket1.on('friend_response', function(data) {
 		console.log("RISPOSTA D'AMICIZIA PER UTENTE CON ID -------> " + data.user_resp_receiver);
 		console.log("IDIDIDIDIDID---> " + receiver_notification[data.user_resp_receiver]);
 		console.log("mia socket --------> " + id_users[data.user_resp_receiver]);
-		socket.to(id_users[data.user_resp_receiver]).emit('friend_resp', {
+		socket1.to(id_users[data.user_resp_receiver]).emit('friend_resp', {
 			news: "Risposta alla richiesta"
 		});
 	});
 });
 
 //utente disconnesso
-io.on('connection', function(socket){
-	socket.on('disconnect', function(){
+io.on('connection', function(socket1){
+	socket1.on('disconnect', function(){
 	    console.log('user disconnected from notification');
 	});
 });
