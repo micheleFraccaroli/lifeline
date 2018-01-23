26bb91ad83ac4cd978630112f4edc4feeaf11b8f
micheleFraccaroli <michelefraccaroli@gmail.com>
2018-01-23 18:26:20 +0100

    Sistemato visualizzazione notifica dei like nei gruppi

--- resources/views/layouts/notifications/like_notification.blade.php
+++ resources/views/layouts/notifications/like_notification.blade.php
@@ -1,9 +1,21 @@
-<a href="/post/{{$notification->data['like']['id_post']}}">
+<?php if($notification->data['like']['group_name'] == null) { ?>
 
-	<?php if(!empty($notification->read_at) != 1) { ?>
-		<h5>Mi piace ad un tuo post da {{ $notification->data['like']['name'] }} {{ $notification->data['like']['surname'] }} <img src="green-dot.svg" height="25" width="28"></h5>  
-	<?php } else { ?>
-		<h5>Mi piace ad un tuo post da {{ $notification->data['like']['name'] }} {{ $notification->data['like']['surname'] }} </h5>
-	<?php } ?>
+	<a href="/post/{{$notification->data['like']['id_post']}}">
+
+		<?php if(!empty($notification->read_at) != 1) { ?>
+			<h5>Mi piace ad un tuo post da {{ $notification->data['like']['name'] }} {{ $notification->data['like']['surname'] }} <img src="green-dot.svg" height="25" width="28"></h5>  
+		<?php } ?>
+
+	</a>
+
+<?php } else { ?>
 	
-</a>
+	<a href="/post/{{$notification->data['like']['id_post']}}">
+
+		<?php if(!empty($notification->read_at) != 1) { ?>
+			<h5>Mi piace ad un tuo post da {{ $notification->data['like']['name'] }} {{ $notification->data['like']['surname'] }} sul gruppo {{  $notification->data['like']['group_name'] }}<img src="green-dot.svg" height="25" width="28"></h5>  
+		<?php } ?>
+
+	</a>
+
+<?php } ?>
