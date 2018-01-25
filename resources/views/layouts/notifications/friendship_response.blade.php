0541c9021c3ce8054a070d246cf1101ef0f5bafe
micheleFraccaroli <michelefraccaroli@gmail.com>
2018-01-25 09:40:29 +0100

    Risolto piccolo bug grammaticale

--- resources/views/layouts/notifications/friendship_response.blade.php
+++ resources/views/layouts/notifications/friendship_response.blade.php
@@ -1,3 +1,3 @@
 <a href="/users/{{$notification->data['user']['my_id']}}">
-		<h5>Risposta alla di amicizia da {{ $notification->data['user']['name'] }} {{ $notification->data['user']['surname'] }}<img src="{{asset('/green-dot.svg')}}" height="25" width="35"></h5>
+		<h5>Risposta alla richiesta di amicizia da {{ $notification->data['user']['name'] }} {{ $notification->data['user']['surname'] }}<img src="{{asset('/green-dot.svg')}}" height="25" width="35"></h5>
 </a>
